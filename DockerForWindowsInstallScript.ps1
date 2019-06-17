6b641b8e0bbad7225925a335ff7075d2cbf32be5
Edson Marques <edson.teixeira@bhs.com.br>
2019-06-17 18:12:54 -0300

    7dockermeetupbhupdate

---
+++ DockerForWindowsInstallScript.ps1
@@ -0,0 +1,31 @@
+﻿# On an online machine, download the zip file.
+Invoke-WebRequest -UseBasicParsing -OutFile docker-18.09.6.zip https://download.docker.com/components/engine/windows-server/18.09/docker-18.09.6.zip
+
+# Stop Docker service
+Stop-Service docker
+    
+# Extract the archive.
+Expand-Archive docker-18.09.6.zip -DestinationPath $Env:ProgramFiles -Force
+
+# Clean up the zip file.
+Remove-Item -Force docker-18.09.6.zip
+
+# Install Docker. This requires rebooting.
+$null = Install-WindowsFeature containers
+
+# Add Docker to the path for the current session.
+$env:path += ";$env:ProgramFiles\docker"
+
+# Optionally, modify PATH to persist across sessions.
+$newPath = "$env:ProgramFiles\docker;" +
+[Environment]::GetEnvironmentVariable("PATH",
+[EnvironmentVariableTarget]::Machine)
+
+[Environment]::SetEnvironmentVariable("PATH", $newPath,
+[EnvironmentVariableTarget]::Machine)
+
+# Register the Docker daemon as a service.
+dockerd --register-service
+
+# Start the Docker service.
+Start-Service docker
